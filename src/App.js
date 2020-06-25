ad0790a3c31d1d09a584a77bd1d0f013b9d0c71d
spidee <spidee@pop-os.localdomain>
2020-06-26 02:33:21 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
     Birvarinder Singh
     mainto: singhbirvarinder@gmail.com
     `);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function callGenApi() {
